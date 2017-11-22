f615e9f35e6b25bc17cb35a0fbddb2215164df46
toby <toby@thedevlins.biz>
2017-11-22 17:09:14 +0000

    Added content, backing up code
    added content to 03.
    added rough notes in 01,06
    copied code from ipynb to initial exploring for easy generation of plots

--- code/initialExploring.py
+++ code/initialExploring.py
@@ -400,3 +400,34 @@ for j in range(len(working_dict)):
     axs[j % 2, j % 3].legend()
 
 f.savefig('plots/mutation_freq/mut_freq all opponents.png')
+
+
+def populationChecker(opponent):
+    # make a nice file name
+    file_name = "data/" + str(opponent).replace(" ", "_").replace(":", "_").lower() + "_pop.csv"
+    # if the file exists dont run, it takes forever, make sure it exists
+    if not os.path.isfile(file_name):
+        df_main = pd.DataFrame(data=None, columns=col_names)
+        for pop_size in populations:
+            start_time = time.clock()
+            pop_run = runGeneticAlgo(opponent,
+                                 population_size=pop_size,
+                                 number_of_game_turns=200,
+                                 cycle_length=200,
+                                 generations=150,
+                                 mutation_probability=0.1,
+                                 reset_file=True)
+        end_time = time.clock()
+        tmp_df = pd.read_csv(pop_run[0], names=col_names)
+        tmp_df["population"] = pop_size
+        tmp_df["time_taken"] = end_time - start_time
+        df_main = df_main.append(tmp_df, ignore_index=True)
+        df_main.to_csv(file_name)
+        print("List Complete:", file_name)
+        return df_main
+    else:
+        print("file already exists, no calcs to do.")
+        file_df = pd.read_csv(file_name)
+        # remove first column
+        file_df = file_df[list(file_df)[1:]]
+        return file_df
