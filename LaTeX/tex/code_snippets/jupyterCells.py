4199f3505b60ee8e9048e477ae191ecf29a03038
Toby Devlin <toby@thedevlins.biz>
2018-03-13 17:31:07 +0000

    Add Content

---
+++ LaTeX/tex/code_snippets/jupyterCells.py
@@ -0,0 +1,18 @@
+
+# -------- CELL 1 --------
+# The ! means 'run this as a bash script' 
+! pip install axelrod
+! pip install git+https://github.com/Axelrod-Python/axelrod-dojo.git
+! wget https://raw.githubusercontent.com/GitToby/FinalYearProject/master/code/full_analysis.py
+
+# -------- CELL 2 --------
+import axelrod as axl
+import full_analysis as fa
+
+run = fa.NewAnalysisRun(population_size=40)
+
+run.add_opponent(axl.TitForTat())
+run.add_opponent(axl.Random())
+run.add_opponent(axl.Grudger())
+
+run.start()
