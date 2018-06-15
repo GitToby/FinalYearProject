8d0411101b3fd28f4bdcba106085485d342ccf09
Toby Devlin <toby@thedevlins.biz>
2018-06-15 10:19:26 +0100

    added presentation & made type changes post VIVA
    Type changes were highlighted during the viva examination.

--- LaTeX/tex/wordcount.bat
+++ LaTeX/tex/wordcount.bat
@@ -1,5 +1,6 @@
 @echo Writing chapter wordcount to wordcount.html ...
 @echo off 
+REM this need to be chaged to where your wordcount perd dist is and your tex folders
 perl C:\Users\toby\Downloads\TeXcount_3_1\texcount.pl -v -html C:\dev\projects\uni\FinalYearProject\LaTeX\tex\chapters\0*.tex > wordcount.html
 REM to run with appendix also, remove the 0* and replace with *
 @echo Done.
